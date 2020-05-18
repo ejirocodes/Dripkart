0dbbf59f83ad70b15782768cf646840c002be742
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-18 11:10:44 +0100

    add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeroMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroMain from './components/header/HeroMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeroMain
   }
 }
 </script>
@@ -21,8 +20,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/header/HeroMain.vue
+++ src/components/header/HeroMain.vue
@@ -13,7 +13,7 @@
 
 <script>
 export default {
-  name: "Navbar"
+  name: "HeroMain"
 };
 </script>
 
