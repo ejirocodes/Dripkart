0d86f0df7d2d8021f78475e3fee50a051ba70965
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-21 10:02:24 +0100

    display featured products

--- src/components/FeaturedProducts.vue
+++ src/components/FeaturedProducts.vue
@@ -4,16 +4,32 @@
       <span class="stroke"></span>
       <h1>{{title}}</h1>
     </div>
+    <figure v-for="(product, index) in featuredProducts" :key="index">
+      <img :src="product.src" alt="product.title" title="product.title" />
+      <figcaption>{{product.title}}</figcaption>
+      <p>&#36;{{product.cost}}</p>
+      <CartButton />
+    </figure>
   </section>
 </template>
 
 <script>
+import products from "../data/products";
+const featuredProducts = products.featured;
+import CartButton from "./CartButton.vue";
 export default {
   name: "FeaturedProducts",
+  components: {
+    CartButton
+  },
   data() {
     return {
-      title: "Featured"
+      title: "Featured",
+      featuredProducts
     };
+  },
+  beforeCreate() {
+    console.log(products);
   }
 };
 </script>
