9d095f488176fdc1149d9aee6f289db8b66e68dd
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-21 10:03:03 +0100

    create add to cart button

---
+++ src/components/CartButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="addToCart()">add to cart</button>
+</template>
+
+<script>
+export default {
+  name: "CartButton",
+  methods: {
+    addToCart() {}
+  }
+};
+</script>
+<style scoped lang="scss">
+@import "../assets/sass/variables.scss";
+@import "../assets/sass/bounce-to-right.scss";
+button {
+  @include hvr-bounce-to-right;
+  padding: 0.8rem 1.5rem;
+  border: none;
+  background: $colour-pri;
+  font-size: 0.8rem;
+  color: #fff;
+  text-transform: uppercase;
+}
+</style>
