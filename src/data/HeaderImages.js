a50e5a42ec609690c32c21c3e353028daad3de49
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-21 10:53:26 +0100

    change images in slider

--- src/data/HeaderImages.js
+++ src/data/HeaderImages.js
@@ -2,18 +2,26 @@ const images = require.context('../assets/images', true, /\.png$/)
 
 const heroImg = {
   image: [
-    {
+     {
+      src: images('./air-vapormax-plus-mens-shoe.png')
+
+    },
+     {
       src: images('./air-max-2090-womens-shoe.png')
 
     },
     {
-      src: images('./nike-red.png'),
+      src: images('./lebron-17-big-kids-shoe.png')
+
     },
     {
       src: images('./air-max-dia-womens-shoe.png')
     },
     {
       src: images('./air-max-97-tortoise-shell-womens-shoe.png')
+    },
+    {
+      src: images('./nike-red.png'),
     },
   ]
 }
