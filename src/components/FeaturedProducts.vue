f11c1b0dbdad8b298064a5e827b34badb0df3893
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-21 10:35:21 +0100

    product showcase title

--- src/components/FeaturedProducts.vue
+++ src/components/FeaturedProducts.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="prod-feature prod-showcase">
     <div class="prod-showcase-heading">
-      <span class="stroke"></span>
+      <!-- <span class="stroke"></span> -->
       <h1>{{title}}</h1>
     </div>
-    <figure v-for="(product, index) in featuredProducts" :key="index">
-      <img :src="product.src" alt="product.title" title="product.title" />
-      <figcaption>{{product.title}}</figcaption>
-      <p>&#36;{{product.cost}}</p>
-      <CartButton />
-    </figure>
+    <section class="products-container">
+      <figure v-for="(product, index) in featuredProducts" :key="index">
+        <img class="product" :src="product.src" :alt="product.title" :title="product.title" />
+        <figcaption>{{product.title}}</figcaption>
+        <p>&#36;{{product.cost}}</p>
+        <CartButton />
+      </figure>
+    </section>
   </section>
 </template>
 
@@ -24,7 +26,7 @@ export default {
   },
   data() {
     return {
-      title: "Featured",
+      title: "What's trending",
       featuredProducts
     };
   },
@@ -33,3 +35,33 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+@import "../assets/sass/variables.scss";
+.prod-showcase {
+  margin: 8rem 0;
+}
+.prod-showcase-heading {
+  h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    text-align: center;
+    text-transform: uppercase;
+    &::before {
+      border-top: 2px solid $colour-pri;
+      display: block;
+      position: relative;
+      top: -25px;
+      margin: 0 auto;
+      width: 10%;
+      content: "";
+    }
+  }
+}
+.products-container {
+  display: flex;
+  figure {
+  }
+  .product {
+  }
+}
+</style>
