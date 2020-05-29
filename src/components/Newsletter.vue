fbaf5acc6d07796be3ea5b75145d5932898db37f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-29 10:34:15 +0100

    newsletter starter

---
+++ src/components/Newsletter.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="newsletter">
+        <div class="newsletter-left">
+            <h2>Subscribe to our newsletter</h2>
+            <p>Subscribe to stay updated with the latest trend in the shoe industry</p>
+        </div>
+        <div class="newsletter-left">
+
+<div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
