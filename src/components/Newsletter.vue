016c45cb0f800864bd31102f6a0c52c6bbb47381
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-29 13:18:49 +0100

    update testimonial

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -2,11 +2,20 @@
   <section class="newsletter">
     <div class="newsletter-left">
       <h2>Subscribe to our newsletter</h2>
-      <p>Subscribe to stay updated with the latest trend in the shoe industry</p>
+      <p>
+        Subscribe to stay updated with the latest
+        <br />trend in the shoe industry
+      </p>
     </div>
-    <div class="newsletter-left">
+    <div class="newsletter-right">
       <FormulateForm v-model="values" @submit="handleLogin">
-        <FormulateInput type="text" name="email" label="Email address" validation="required|email" />
+        <FormulateInput
+          type="text"
+          name="email"
+          placeholder="name@name.com"
+          aria-placeholder="myname@example.com"
+          validation="required|email"
+        />
         <FormulateInput type="submit" label="Login" />
       </FormulateForm>
     </div>
@@ -16,7 +25,52 @@
 <script>
 export default {
   data() {
-    return { values: {}, handleLogin: () => alert("Logged in") };
+    return {
+      values: {},
+      handleLogin: () => alert("Logged in")
+    };
   }
 };
 </script>
+
+<style scoped lang="scss">
+@import "../assets/sass/variables.scss";
+.newsletter {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 4rem;
+  background-color: $colour-pri;
+  padding: 1rem 3rem;
+  .newsletter-left {
+    h2 {
+      text-transform: capitalize;
+      font-weight: 600;
+      margin-bottom: $gutter-1;
+    }
+    p {
+      font-size: 1rem;
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.newsletter {
+  .newsletter-right {
+    form {
+      display: flex;
+    }
+    .formulate-input[data-classification="text"] input {
+      font-size: 1rem !important;
+    }
+    .formulate-input[data-classification="button"] button {
+      font-size: 1rem;
+      padding: .75em 1.8rem;
+    }
+    .formulate-input {
+        margin-bottom: 0;
+    }
+  }
+}
+</style>
