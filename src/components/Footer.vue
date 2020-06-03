3c8c19664651a64adb485b0f81ed8ee7674fba65
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-03 12:59:15 +0100

    basic footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <c-link href="https://github.com/Ejiro-Asiuwhu/dripkart" is-external>
+      View on GitHub
+      <c-icon name="external-link-alt" mx="2px" />
+    </c-link>
+  </div>
+</template>
+
+<script>
+import { CLink } from "@chakra-ui/vue";
+export default {
+  components: {
+    CLink
+  }
+};
+</script>
