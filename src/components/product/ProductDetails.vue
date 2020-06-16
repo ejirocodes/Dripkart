c0c665e13281728591fa0e1125a2ac26c5c12472
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-16 15:05:40 +0100

    product details item positioned ✌✌✌

--- src/components/product/ProductDetails.vue
+++ src/components/product/ProductDetails.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <h1 class="product-title">{{product.title }}</h1>
-    <img class="product-img" :src="product.src" :alt="product.src" :title="product.title" />
-    <p class="product-title">{{product.descriotion}}</p>
-    <p class="product-cost">{{product.cost}}</p>
-  </div>
+  <section class="prod-det">
+    <div class="prod-det-left">
+      <h1 class="product-title">{{product.title }}</h1>
+      <p class="product-desc">{{product.description}}</p>
+      <p class="product-cost">&#36;{{product.cost}}</p>
+      <button>
+        <i class="fas fa-shopping-cart"></i> Add to cart
+      </button>
+    </div>
+    <div class="prod-det-right">
+      <img class="product-img" :src="product.src" :alt="product.title" :title="product.title" />
+    </div>
+  </section>
 </template>
 
 <script>
@@ -16,16 +23,31 @@ export default {
   computed: {
     product() {
       const slug = this.$route.params.slug;
-      return featuredProduct.find(prod => prod.slug === slug) || newArrival.find(prod => prod.slug === slug);
+      return (
+        featuredProduct.find(prod => prod.slug === slug) ||
+        newArrival.find(prod => prod.slug === slug)
+      );
     }
   }
 };
 </script>
 <style scoped lang="scss">
-div {
+.prod-det {
   font-size: 3rem !important;
-}
-img {
-  width: 15rem;
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  .prod-det-right,
+  .prod-det-left {
+    width: 50vw;
+  }
+  .prod-det-right {
+    .product-img {
+      width: 100%;
+    }
+  }
+  .product-title {
+    font-size: 4rem;
+    font-weight: 700;
+  }
 }
 </style>
