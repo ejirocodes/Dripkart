527d3b9fddcc7d4b3b0ee3a29e684d9907bb341d
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-17 11:16:46 +0100

    fully responsive ProductDetails component 🤙

--- src/components/product/ProductDetails.vue
+++ src/components/product/ProductDetails.vue
@@ -15,7 +15,9 @@
         <img class="product-img" :src="product.src" :alt="product.title" :title="product.title" />
       </div>
     </div>
-    <router-link class="more-link" to="/shop">Discover Everything</router-link>
+    <p>
+      <router-link class="more-link" to="/shop">Discover Everything</router-link>
+    </p>
   </section>
 </template>
 
@@ -47,7 +49,7 @@ export default {
   height: 80vh;
   .prod-det-right,
   .prod-det-left {
-    width: 50vw;
+    width: 100%;
   }
   .prod-det-right {
     transition: all cubic-bezier(0.455, 0.03, 0.515, 0.955) 0.5s;
@@ -62,6 +64,7 @@ export default {
     }
   }
   .prod-det-left {
+    margin-right: 1rem;
     .cart-btn-2 {
       &:hover {
         &:after {
@@ -75,7 +78,6 @@ export default {
       border-radius: 40px;
       font-family: 'Lato', sans-serif;
       padding: 0.8rem 2.5rem;
-      display: inline-block;
       vertical-align: middle;
       transform: perspective(1px) translateZ(0);
       box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
@@ -88,7 +90,6 @@ export default {
       color: #fff;
       text-transform: uppercase;
       overflow: hidden;
-      position: relative;
       display: flex;
       span {
         z-index: 20;
@@ -138,5 +139,40 @@ export default {
   font-family: 'Lato', sans-serif;
   letter-spacing: 1.2px;
   color: $colour-uni;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+}
+
+// MEDIA QUERIES
+@media screen and (max-width: 768px) {
+  .prod-det {
+    flex-direction: column-reverse;
+    height: 100%;
+    .prod-det-left {
+      margin-right: 0;
+    }
+    .prod-det-right {
+      margin-bottom: 2rem;
+    }
+  }
+}
+@media screen and (max-width: 425px) {
+  .prod-det {
+    .prod-det-left {
+      .product-title {
+        font-size: 2rem;
+      }
+      .product-desc {
+        font-size: 1rem;
+      }
+      .cart-btn-2 {
+            padding: 0.7rem 1.8rem;
+      }
+    }
+  }
+}
+.more-link {
+  display: none;
 }
 </style>
