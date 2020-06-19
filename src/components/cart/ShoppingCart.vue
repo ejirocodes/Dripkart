eff95bd6c53d9e309d4e3c4b5eb4aa9503c4e5c2
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-19 15:25:01 +0100

    cart right style

--- src/components/cart/ShoppingCart.vue
+++ src/components/cart/ShoppingCart.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="cart-page">
-    <div class="cart-left" v-for="(product, index) in cart" :key="index =+ 'new'">
+    <div class="cart-left-item">
+ <div class="cart-left" v-for="(product, index) in cart" :key="index =+ 'new'">
       <div class="cart-img">
         <img :src="product.src" :alt="product.title" :title="product.title" />
       </div>
@@ -12,7 +13,9 @@
         <p>$ {{product.cost}}</p>
       </div>
     </div>
-    <div class="cart-left" v-if="cart.length">
+    </div>
+   
+    <div class="cart-right" v-if="cart.length">
       <div class="cart-left-top">
         <h2>Summary</h2>
         <p>You have {{cart.length}} items in your cart</p>
@@ -22,7 +25,10 @@
         <p>Total Cost{{cart.cost}} items in your cart</p>
       </div>
     </div>
-    <div v-else>Your cart is empty</div>
+    <div v-else>
+      <p>Your cart is empty</p>
+      <router-link to="/shop">Go to shop</router-link>
+    </div>
   </section>
 </template>
 
@@ -41,10 +47,50 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.cart-pag {
+@import '../../assets/sass/_variables.scss';
+.cart-page {
   display: flex;
-}
-img {
-  width: 100%;
+  justify-content: space-around;
+  .cart-left {
+    display: flex;
+    align-items: center;
+    .cart-img {
+      width: 20rem;
+      margin-right: 4rem;
+      img {
+        width: 100%;
+      }
+    }
+    .product-title {
+      margin-right: 2rem;
+      h1 {
+        font-size: 1.6rem;
+        font-weight: 700;
+        color: $colour-pri;
+      }
+      p {
+        font-size: 1.2rem;
+        font-weight: 400;
+      }
+    }
+    .product-cost {
+          font-size: 2rem;
+    font-weight: 700;
+    }
+  }
+  .cart-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .cart-left-top {
+      h2 {
+        font-size: 2rem;
+        margin-bottom: 2rem;
+      }
+      p {
+        font-size: .8rem;
+      }
+    }
+  }
 }
 </style>
