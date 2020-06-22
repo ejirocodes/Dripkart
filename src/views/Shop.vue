8abc892ed83d9fc2fcc0d5b30b0120f4b3527157
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-22 21:41:40 +0100

    add to cart button

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -6,6 +6,43 @@
         <img class="shop-img" :src="product.src" :alt="product.title" :title="product.title" />
         <h2 class="shop-title">{{product.title}}</h2>
         <p class="shop-cost">&#36;{{product.cost}}</p>
+        <button class="add-to-cart">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 512 512"
+            style="enable-background:new 0 0 512 512;"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"
+                />
+              </g>
+            </g>
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+          </svg>
+        </button>
       </figure>
       <figure v-for="(product) in  featuredProducts" :key="product.slug">
         <img class="shop-img" :src="product.src" :alt="product.title" :title="product.title" />
@@ -45,11 +82,22 @@ export default {
     margin-top: 2rem;
     padding: 0 2rem;
     figure {
-      width: 100%;
-      height: 28rem;
       padding: 0.5rem;
       border-radius: 1rem;
       background-color: #fff;
+      .add-to-cart {
+        float: right;
+        background: transparent;
+        border: 1px solid #949494;
+        padding: 0.5rem 0.6rem;
+        border-radius: 1rem;
+        svg {
+          width: 2rem;
+          float: right;
+          margin: 0;
+          fill: #949494;
+        }
+      }
     }
     .shop-img {
       max-width: 100%;
@@ -58,6 +106,9 @@ export default {
       height: 20rem;
       padding: 1.4rem;
       border-radius: 1rem;
+    }
+    .shop-title {
+      margin: 1rem 0;
     }
   }
 }
