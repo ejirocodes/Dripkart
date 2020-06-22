b93625d5e62372fd414b669aea983e24ad2e8f8d
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-22 18:46:14 +0100

    set shop product layout

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="shop">
+  <section class="shop-container">
     <h1>{{title}}</h1>
-    <div class="shop-container">
+    <div class="shop">
       <figure v-for="(product) in  newArrival" :key="product.slug">
-        <img :src="product.src" :alt="product.title" :title="product.title" />
-        <h2>{{product.title}}</h2>
-        <p>&#36;{{product.cost}}</p>
+        <img class="shop-img" :src="product.src" :alt="product.title" :title="product.title" />
+        <h2 class="shop-title">{{product.title}}</h2>
+        <p class="shop-cost">&#36;{{product.cost}}</p>
       </figure>
       <figure v-for="(product) in  featuredProducts" :key="product.slug">
-        <img :src="product.src" :alt="product.title" :title="product.title" />
+        <img class="shop-img" :src="product.src" :alt="product.title" :title="product.title" />
         <h2>{{product.title}}</h2>
         <p>&#36;{{product.cost}}</p>
       </figure>
@@ -31,5 +31,16 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.shop-container {
+  .shop {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+    grid-gap: 20px;
+    align-items: stretch;
+    .shop-img {
+     max-width: 100%;   
+    }
+  }
+}
 </style>
