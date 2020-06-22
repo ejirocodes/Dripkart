1a9614aad859e0a6a17175f826a8133dad7029be
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-22 18:04:10 +0100

    fix duplicate keys

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -2,11 +2,15 @@
   <section class="shop">
     <h1>{{title}}</h1>
     <div class="shop-container">
-      <figure v-for="(product, index) in  newArrival" :key="index">
+      <figure v-for="(product) in  newArrival" :key="product.slug">
         <img :src="product.src" :alt="product.title" :title="product.title" />
+        <h2>{{product.title}}</h2>
+        <p>&#36;{{product.cost}}</p>
       </figure>
-      <figure v-for="(product, index) in  featuredProducts" :key="index">
+      <figure v-for="(product) in  featuredProducts" :key="product.slug">
         <img :src="product.src" :alt="product.title" :title="product.title" />
+        <h2>{{product.title}}</h2>
+        <p>&#36;{{product.cost}}</p>
       </figure>
     </div>
   </section>
@@ -19,7 +23,7 @@ const newArrival = products.newArrivals;
 export default {
   data() {
     return {
-      title: 'Shop',
+      title: 'Discover the best',
       featuredProducts,
       newArrival
     };
