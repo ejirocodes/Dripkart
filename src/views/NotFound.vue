3f22dfe1ed37990af0a2038e643f459467a94f96
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-29 09:43:07 +0100

    update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,21 +1,22 @@
 <template>
   <section class="not-found">
-      <h1>Lost in space?</h1>
-      <div class="not-found-img">
-          <img src="" alt="">
-      </div>
-      <router-link to="/">Home</router-link>
+    <h1>Lost in space?</h1>
+    <div class="not-found-img">
+      <img src="../assets/svg/404.svg" alt="404" />
+    </div>
+    <div class="not-found-info">
+      <p>The page you requested could not be found</p>
+      <router-link to="/">Back to Home</router-link>
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-    h1 {
-        font-size: 3rem;
-    }
+h1 {
+  font-size: 3rem;
+}
 </style>
